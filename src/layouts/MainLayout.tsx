9131c4af5d76f405c6d4ed1675cfec760cf61137
Andres <andresrojo20013@gmail.com>
2026-06-26 16:56:16 -0500

    chore: initialize portfolio architecture

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+interface Props{
+    children:ReactNode;
+}
+
+export default function MainLayout({children}:Props){
+
+    return(
+        <main>
+
+            {children}
+
+        </main>
+    )
+
+}
