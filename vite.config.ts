da6ec568343350ef475d16abf70d076b779755b3
Andres <andresrojo20013@gmail.com>
2026-06-29 17:17:48 -0500

    vite se configura

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
     tailwindcss(),
     tsconfigPaths(),
   ],
